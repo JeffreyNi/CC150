a0090e5f5fd1b6ee922c423868586ae6222431f8
JeffreyNi <njf19920206@163.com>
2016-01-25 15:13:51 -0800

    isBalanced

---
+++ Chapter4/Chapter4BalancedTree.java
@@ -0,0 +1,41 @@
+/********************************************************
+ * This class implements problem 4.1 in CC150.
+ *
+ * Implement a function to check if a tree is balanced. For
+ * the purpose of this question, a balanced tree is defined
+ * to be a tree such that no two leaf nodes differ in distance
+ * from the root by more than one.
+ *
+ * @author Jiafeng Ni
+ * 
+ ********************************************************/
+
+public class Chapter4BalancedTree{
+
+    public class TreeNode {
+	private int val;
+	private TreeNode left;
+	private TreeNode right;
+
+	public TreeNode (int val) {
+	    this.val = val;
+	}
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+	return getHeight(root) != -1;
+    }
+
+
+    private static int getHeight(TreeNode root) {
+	if (root == null) {return 0;}
+
+	int leftHeight = getHeight(root.left);
+	int rightHeight = getHeight(root.right);
+
+	if (leftHeight == -1 || rightHeight == -1) { return -1; }
+	if (Math.abs(leftHeight - rightHeight) > 1) { return -1; }
+	
+	return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
